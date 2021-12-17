bcd7d31052d57016478e0afa121dd0db0d702c5e
Noah Taheij <noah@taheij.nl>
2021-12-17 06:26:40 +0100

    Day17

--- py/day17.py
+++ py/day17.py
@@ -1,39 +1,43 @@
-#!/usr/bin/python3
-import sys
-import heapq
-import itertools
-from collections import defaultdict, Counter, deque
-
-#target area: x=96..125, y=-144..-98
-
-# y(t) ~ -t^2 + DY*t
-
-p2 = 0
-ans = 0
-for DX in range(150):
-    for DY in range(-150, 1000):
-        ok = False
-        max_y = 0
-        x = 0
-        y = 0
-        dx = DX
-        dy = DY
-        for t in range(1000):
-            x += dx
-            y += dy
-            max_y = max(max_y, y)
-            if dx > 0:
-                dx -= 1
-            elif dx < 0:
-                dx += 1
-            dy -= 1
-            #if 20<=x<=30 and -10<=y<=-5:
-            if 150<=x<=193 and -136<=y<=-86:
-                ok = True
-        if ok:
-            p2 += 1
-            if max_y > ans:
-                ans = max_y
-                print(DX,DY,ans)
-print(ans)
-print(p2)
+import numpy as np
+ 
+def part_1():
+  #target area: x=150..193, y=-136..-86
+  x_min = 150
+  x_max = 193
+  y_min = -136
+  y_max = -86
+  total = 0
+  best_y = 0
+  for sx in range(0,x_max+1):
+    for sy in range(y_min,1000):
+      x = 0
+      y = 0
+      x_vel = sx
+      y_vel = sy
+      max_y = 0
+      while x <= x_max and y >= y_min:
+        x_vel,y_vel,x,y = step(x_vel,y_vel,x,y)
+        if y>max_y:
+          max_y = y
+        if x >= x_min and x <= x_max and y>=y_min and y<=y_max:
+          total+=1
+          if max_y > best_y:
+            best_y = max_y
+          break
+        if x_vel == 0 and x < x_min:
+          break
+  print(best_y)
+  print(total)
+ 
+#take one step
+def step(x_vel,y_vel,x,y):
+  x+=x_vel
+  y+=y_vel
+  if x_vel > 0:
+    x_vel -= 1
+  elif x_vel < 0:
+    x_vel += 1
+  y_vel -= 1
+  return x_vel,y_vel,x,y
+ 
+part_1()
